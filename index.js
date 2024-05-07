b0c2e3c62597eaadcc4c56960208ed87b1036e9f
tungbeo9090 <legendzacknam1311@outlook.com>
2024-05-08 06:52:43 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-function permuteUnique(nums) {
-  nums.sort((a, b) => a - b);
-  const result = [];
-  const used = new Array(nums.length).fill(false);
-  backtrack([]);
-  return result;
-  function backtrack(permutation) {
-    if (permutation.length === nums.length) {
-      result.push([...permutation]);
-      return;
+function isValidBST(root) {
+  const stack = [];
+  let inorder = -Infinity;
+  while (stack.length || root) {
+    while (root) {
+      stack.push(root);
+      root = root.left;
     }
-    for (let i = 0; i < nums.length; i++) {
-      if (used[i] || (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]))
-        continue;
-      used[i] = true;
-      permutation.push(nums[i]);
-      backtrack(permutation);
-      permutation.pop();
-      used[i] = false;
-    }
+    root = stack.pop();
+    if (root.val <= inorder) return false;
+    inorder = root.val;
+    root = root.right;
   }
+  return true;
 }
