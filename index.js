214d041bba16fd7a79f3db147c0fda9693e8dcb5
tungbeo9090 <legendzacknam1311@outlook.com>
2024-05-09 00:11:32 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-function isValidBST(root) {
-  const stack = [];
-  let inorder = -Infinity;
-  while (stack.length || root) {
-    while (root) {
-      stack.push(root);
-      root = root.left;
-    }
-    root = stack.pop();
-    if (root.val <= inorder) return false;
-    inorder = root.val;
-    root = root.right;
+const factorialRecursive = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
   }
-  return true;
-}
+  return n * factorialRecursive(n - 1);
+};
