b884d454be6aff6d5e2f312a9139cac862c2bd7d
tungbeo9090 <legendzacknam1311@outlook.com>
2024-05-09 19:16:10 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const factorialRecursive = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  return n * factorialRecursive(n - 1);
-};
+const flatten = (arr) => arr.reduce((a, v) => a.concat(v), []);
